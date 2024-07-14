5f52d08b530c521608218138b602e2ff45ca43a1
shakhzoda05 <shakhzoda05@gmail.com>
2024-07-14 06:24:32 +0500

    the end

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,11 @@
 let elUserName=document.querySelector(".username")
-
 const userData=JSON.parse(window.localStorage.getItem("userData"))
 elUserName.textContent=userData.username
+
+function handleClickLogOutBtn(){
+    let elConfirmLogOut=confirm("Do you want to log out?!")
+    if(elConfirmLogOut){
+        location.pathname="index.html"
+        window.localStorage.clear()
+    }
+}
