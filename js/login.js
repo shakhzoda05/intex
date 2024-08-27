7fd84293312659006ee55ab51bd0149e091269ff
shakhzoda05 <shakhzoda05@gmail.com>
2024-08-28 04:24:14 +0500

    finished

--- js/login.js
+++ js/login.js
@@ -1,14 +1,28 @@
-let elLoginForm=document.querySelector(".login-form")
-elLoginForm.addEventListener("submit", function(e){
-    e.preventDefault()
-    const data={
-        username:e.target.username.value,
-        password:e.target.password.value
+let elLoginForm = document.querySelector(".login-form");
+
+elLoginForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const data = {
+        username: e.target.username.value,
+        password: e.target.password.value
+    };
+
+    const defaultUser = {
+        username: "shaxzoda",
+        password: "123"
+    };
+
+    const registeredUser = JSON.parse(localStorage.getItem("registeredUser"));
+
+    if (
+        (data.username === defaultUser.username && data.password === defaultUser.password) ||
+        (registeredUser && data.username === registeredUser.username && data.password === registeredUser.password)
+    ) {
+        location.pathname = "./admin.html";
+    } else {
+        alert("Xato ma'lumot kiritildi!");
     }
-    if(data.username=="shaxzoda" && data.password=="123"){
-    location.pathname="./admin.html" 
-    }
-    else(
-        alert("Xato malumot kiritildi!")
-    )
-})
+});
+
+
--- js/register.js
+++ js/register.js
@@ -1,11 +1,17 @@
-let elRegisterForm=document.querySelector(".register-form")
+let elRegisterForm = document.querySelector(".register-form");
 
-elRegisterForm.addEventListener("submit", function(e){
-    e.preventDefault()
-    const newData={
-        newUsername:e.target.username.value,
-        newPassword:e.target.password.value
-    }
-    localStorage.setItem("isRegistered",JSON.stringify(newData))
-    setTimeout(()=>location.pathname="/",1000)
-})
+elRegisterForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const newData = {
+        username: e.target.username.value,
+        password: e.target.password.value
+    };
+
+    localStorage.setItem("registeredUser", JSON.stringify(newData));
+    alert("Ro'yxatdan o'tish muvaffaqiyatli o'tdi!");
+
+    setTimeout(() => location.pathname = "/", 1000);
+});
+
+
